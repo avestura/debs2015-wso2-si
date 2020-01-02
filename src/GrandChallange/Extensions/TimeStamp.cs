1c8878b060ab11724cb0a5f216ed5a29aab2b947
Ahmad <faazeli.ahmad@gmail>
2020-01-02 15:01:39 +0330

    Add TimeStamp class.

---
+++ src/GrandChallange/Extensions/TimeStamp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GrandChallange.Extensions
+{
+    public class TimeStamp
+    {
+        public DateTime dateTime { get; set; }
+
+        public TimeStamp(DateTime newDateTime) 
+        {
+            if (dateTime == null)
+            {
+                throw new ArgumentNullException(nameof(newDateTime));
+            }
+
+            dateTime = newDateTime;
+        }
+
+        public long GetUnixTime() 
+        {          
+            var dateTimeOffset = new DateTimeOffset(dateTime).ToUniversalTime();
+            return dateTimeOffset.ToUnixTimeMilliseconds();
+        }
+    }
+}

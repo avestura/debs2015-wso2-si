57135ff509c66f229994f49235a153a623b9dd78
Ahmad <faazeli.ahmad@gmail>
2020-01-02 17:41:59 +0330

    Define FirstQueryInputModel.

---
+++ src/GrandChallange/Models/FirstQueryInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GrandChallange.Models
+{
+    public class FirstQueryInputModel
+    {
+        public long PickUpTime { get; set; }
+        public long DropOffTime { get; set; }
+        public string PickCellId { get; set; }
+        public string DropCellId { get; set; }
+        public long EventTimeStamp { get; set; }
+        public string PickUpTimeOrig { get; set; }
+        public string DropOffTimeOrig { get; set; }
+    }
+}

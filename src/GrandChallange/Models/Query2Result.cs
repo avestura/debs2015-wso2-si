898e05167d2b914071c603cd1132aa027640b1ae
ZahraAkhgari <akhgari.zahra@gmail.com>
2020-01-02 16:03:33 +0330

    Add Q2 Result model

---
+++ src/GrandChallange/Models/Query2Result.cs
@@ -0,0 +1,148 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrandChallange.Models
+{
+    class Query2Result
+    {
+        [Index(0)]
+        public string PickupDatetime { get; set; }
+
+        [Index(1)]
+        public string DropoffDatetime { get; set; }
+
+        [Index(2)]
+        public string ProfitableCellId1 { get; set; }
+
+        [Index(3)]
+        public float EmptyTaxiesInCellId1 { get; set; }
+    
+        [Index(4)]
+        public float MedianProfitInCellId1 { get; set; }
+
+        [Index(5)]
+        public float ProfitabilityOfCell1 { get; set; }
+
+        [Index(6)]
+        public string ProfitableCellId2 { get; set; }
+
+        [Index(7)]
+        public float EmptyTaxiesInCellId2 { get; set; }
+
+        [Index(8)]
+        public float MedianProfitInCellId2 { get; set; }
+
+        [Index(9)]
+        public float ProfitabilityOfCell2 { get; set; }
+
+        [Index(10)]
+        public string ProfitableCellId3 { get; set; }
+
+        [Index(11)]
+        public float EmptyTaxiesInCellId3 { get; set; }
+
+        [Index(12)]
+        public float MedianProfitInCellId3 { get; set; }
+
+        [Index(13)]
+        public float ProfitabilityOfCell3 { get; set; }
+
+        [Index(14)]
+        public string ProfitableCellId4 { get; set; }
+
+        [Index(15)]
+        public float EmptyTaxiesInCellId4 { get; set; }
+
+        [Index(16)]
+        public float MedianProfitInCellId4 { get; set; }
+
+        [Index(17)]
+        public float ProfitabilityOfCell4 { get; set; }
+
+        [Index(18)]
+        public string ProfitableCellId5 { get; set; }
+
+        [Index(19)]
+        public float EmptyTaxiesInCellId5 { get; set; }
+
+        [Index(20)]
+        public float MedianProfitInCellId5 { get; set; }
+
+        [Index(21)]
+        public float ProfitabilityOfCell5 { get; set; }
+
+        [Index(22)]
+        public string ProfitableCellId6 { get; set; }
+
+        [Index(23)]
+        public float EmptyTaxiesInCellId6 { get; set; }
+
+        [Index(24)]
+        public float MedianProfitInCellId6 { get; set; }
+
+        [Index(25)]
+        public float ProfitabilityOfCell6 { get; set; }
+
+        [Index(26)]
+        public string ProfitableCellId7 { get; set; }
+
+        [Index(27)]
+        public float EmptyTaxiesInCellId7 { get; set; }
+
+        [Index(28)]
+        public float MedianProfitInCellId7 { get; set; }
+
+        [Index(29)]
+        public float ProfitabilityOfCell7 { get; set; }
+
+        [Index(30)]
+        public string ProfitableCellId8 { get; set; }
+
+        [Index(31)]
+        public float EmptyTaxiesInCellId8 { get; set; }
+
+        [Index(32)]
+        public float MedianProfitInCellId8 { get; set; }
+
+        [Index(33)]
+        public float ProfitabilityOfCell8 { get; set; }
+
+        [Index(34)]
+        public string ProfitableCellId9 { get; set; }
+
+        [Index(35)]
+        public float EmptyTaxiesInCellId9 { get; set; }
+
+        [Index(36)]
+        public float MedianProfitInCellId9 { get; set; }
+
+        [Index(37)]
+        public float ProfitabilityOfCell9 { get; set; }
+
+        [Index(38)]
+        public string ProfitableCellId10 { get; set; }
+
+        [Index(39)]
+        public float EmptyTaxiesInCellId10 { get; set; }
+
+        [Index(40)]
+        public float MedianProfitInCellId10 { get; set; }
+
+        [Index(41)]
+        public float ProfitabilityOfCell10 { get; set; }
+
+        [Index(42)]
+        public string delay { get; set; }
+
+
+
+
+
+
+
+
+
+    }
+}

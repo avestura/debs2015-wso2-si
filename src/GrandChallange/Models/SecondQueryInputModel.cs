9a00d0bff42ac6330804e749e873d4ca22dcd0cd
ZahraAkhgari <akhgari.zahra@gmail.com>
2020-01-04 11:38:19 +0330

    Add Second Query Input Model

---
+++ src/GrandChallange/Models/SecondQueryInputModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrandChallange.Models
+{
+    class SecondQueryInputModel
+    {
+        public string Medallion { get; set; }
+        public long PickupTime { get; set; }
+        public long DropoffTime { get; set; }
+        public string PickCell { get; set; }
+        public string DropCell { get; set; }
+        public long EventTimestamp { get; set; }
+        public string PickupTimeOrig { get; set; }
+        public string DropoffTimeOrig { get; set; }
+        public float FareAmount { get; set; }
+        public float TipAmount { get; set; }
+    }
+}

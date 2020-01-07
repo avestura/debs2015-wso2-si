2a1f961146adacbb0e54d9c9dd2f1840cc9cdbb1
Ahmad <faazeli.ahmad@gmail>
2020-01-07 15:11:34 +0330

    Changes for query 2.

--- src/GrandChallange.EventWebService/Controllers/Query2MedianController.cs
+++ src/GrandChallange.EventWebService/Controllers/Query2MedianController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using GrandChallange.EventWebService.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MathNet.Numerics.Statistics;
 
 namespace GrandChallange.EventWebService.Controllers
 {
@@ -13,17 +15,23 @@ namespace GrandChallange.EventWebService.Controllers
     [ApiController]
     public class Query2MedianController : ControllerBase
     {
-        public static List<(float profit, long timestamp)> Data { get; set; }
-            = new List<(float, long)>();
+        //public static List<(float profit, long timestamp)> Data { get; set; }
+        //    = new List<(float, long)>();
 
+        public static ConcurrentDictionary<float, long> Data { get; set; } = new ConcurrentDictionary<float, long>();
 
         [HttpPost]
         public object Post(Wso2Request<Wso2Model> input)
         {
             var ts = input.Event.Now;
-            Data = Data.Where(x => x.timestamp > ts - 900000).ToList();
 
-            Data.Add((input.Event.CurrentProfit, input.Event.Now));
+            foreach (var item in Data)
+            {
+                if (item.Value > ts - 900000)
+                    Data.TryRemove(item.Key, out long x);
+            }
+
+            Data[input.Event.CurrentProfit] = input.Event.Now;
 
             return new
             {
@@ -33,9 +41,15 @@ namespace GrandChallange.EventWebService.Controllers
 
         private float Median()
         {
-            var ys = Data.OrderBy(x => x.profit).ToList();
-            double mid = (ys.Count - 1) / 2.0;
-            return (ys[(int)(mid)].profit + ys[(int)(mid + 0.5)].profit) / 2;
+            try
+            {
+                return Data.OrderBy(x => x.Key).Select(x => x.Key).Median();
+            }
+            catch (Exception)
+            {
+
+                return 0;
+            }
         }
 
         public class Wso2Model
